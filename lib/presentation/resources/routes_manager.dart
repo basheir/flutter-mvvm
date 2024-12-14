9be0da5fa5ccafb1ae4f52173992acc3350d63c0
Bashir Mahad <67736569+basheir@users.noreply.github.com>
2024-12-14 22:18:42 +0300

    finished adding routes manager

--- lib/presentation/resources/routes_manager.dart
+++ lib/presentation/resources/routes_manager.dart
@@ -1,3 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:mvvm_project/presentation/forgot_password/forgot_password.dart';
+import 'package:mvvm_project/presentation/login/login.dart';
+import 'package:mvvm_project/presentation/main/main_view.dart';
+import 'package:mvvm_project/presentation/register/register.dart';
+import 'package:mvvm_project/presentation/splash/splash.dart';
+import 'package:mvvm_project/presentation/store_details/store_details.dart';
+
 class Routes {
   static const String splashRoute = '/';
   static const String onBoardingRoute = '/onBoarding';
@@ -6,4 +14,38 @@ class Routes {
   static const String forgotPasswordRoute = '/forgotPassword';
   static const String mainRoute = '/main';
   static const String storeDetailsRoute = '/storeDetails';
+}
+
+class RouteGenerator {
+  static Route<dynamic> getRoute(RouteSettings routeSettings) {
+    switch (routeSettings) {
+      case Routes.splashRoute:
+        return MaterialPageRoute(builder: (_) => SplashView());
+      case Routes.loginRoute:
+        return MaterialPageRoute(builder: (_) => LoginView());
+      case Routes.registerRoute:
+        return MaterialPageRoute(builder: (_) => RegisterView());
+      case Routes.forgotPasswordRoute:
+        return MaterialPageRoute(builder: (_) => ForgotPasswordView());
+      case Routes.mainRoute:
+        return MaterialPageRoute(builder: (_) => MainView());
+      case Routes.storeDetailsRoute:
+        return MaterialPageRoute(builder: (_) => StoreDetailsView());
+      default:
+        return undefinedRoute();
+    }
+  }
+
+  static Route undefinedRoute() {
+    return MaterialPageRoute(
+      builder: (_) => Scaffold(
+        appBar: AppBar(
+          title: const Text('No route found'),
+        ),
+        body: const Center(
+          child: Text('No route found'),
+        ),
+      ),
+    );
+  }
 }
