168185fc8e626bb2107ca6975be29f68e132e43c
Bashir Mahad <67736569+basheir@users.noreply.github.com>
2024-12-14 08:05:05 +0300

    appbar theme added

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,6 +1,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:mvvm_project/presentation/color_manager.dart';
+import 'package:mvvm_project/presentation/font_manager.dart';
+import 'package:mvvm_project/presentation/style_manager.dart';
 import 'package:mvvm_project/presentation/values_manager.dart';
 
 ThemeData getApplicationTheme (){
@@ -10,12 +12,24 @@ ThemeData getApplicationTheme (){
     primaryColorLight: ColorManager.primaryOpacity70,
     primaryColorDark: ColorManager.darkPrimary,
     disabledColor: ColorManager.grey1,
+    // ripple color
+    splashColor: ColorManager.grey,
 
     // card view theme 
     cardTheme: CardTheme(
       color: ColorManager.white,
       shadowColor: ColorManager.grey,
       elevation: AppSize.s4
+    ),
+
+    appBarTheme: AppBarTheme(
+      centerTitle: true,
+      color: ColorManager.primary,
+      elevation: AppSize.s4,
+      shadowColor: ColorManager.primaryOpacity70,
+      titleTextStyle: getRegularStyle(color: ColorManager.white, fontSize: FontSize.s16)
     )
   );
+
+  // app bar theme
 }
