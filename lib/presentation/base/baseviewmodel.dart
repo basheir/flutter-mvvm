9b207494659a6b756434993a1126bc330e03463d
Bashir Mahad <67736569+basheir@users.noreply.github.com>
2024-12-24 07:34:20 +0300

    Add base view model classes for shared properties and methods

---
+++ lib/presentation/base/baseviewmodel.dart
@@ -0,0 +1,12 @@
+abstract class BaseViewModel {
+  // shared properties and methods that will be used by all view models
+}
+
+abstract class BaseviewmodelIput {
+  void init(); // This will be called when the view model is created
+  void dispose(); // This will be called when the view model is destroyed
+}
+
+abstract class BaseviewmodelOutput {
+  
+}
