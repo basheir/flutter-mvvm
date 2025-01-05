c22253a900190d0bb579ec49f98688ab5a0ac15d
Bashir Mahad <67736569+basheir@users.noreply.github.com>
2025-01-05 08:00:00 +0300

    Add Contact, Customer, and Authentication models for user data management

--- lib/domain/model.dart
+++ lib/domain/model.dart
@@ -6,3 +6,30 @@ class SliderObject {
 
   SliderObject(this.title, this.subTitle, this.image);
 }
+
+class Contact{
+  String email;
+  String phone;
+  String link;
+
+  Contact({required this.email, required this.phone, required this.link});
+}
+
+
+class Customer{
+  int? id;
+  String? name;
+  String? numOfNotifications;
+
+  Customer({this.id, this.name, this.numOfNotifications});
+}
+
+
+class Authentication{
+  int? status;
+  String? message;
+  Customer? customer;
+  Contact? contacts;
+
+  Authentication({this.status, this.message, this.customer, this.contacts});
+}
